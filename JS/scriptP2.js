281f4516020065e98a4800ea359c21ea8af606d9
ChuckleDusters <thatoneheros451@gmail.com>
2020-10-20 23:49:44 -0600

    logic fix

--- JS/scriptP2.js
+++ JS/scriptP2.js
@@ -172,7 +172,7 @@ function shuffle() {
 
 function audioLooper(counter) {
 	console.log("Called audioLooper at counter = " + counter);
-	if (shuffleVar == false && autoplayVar == true) {
+	if (shuffleVar == false) {
 		counter = songArray[counter];
 		console.log("Counter set to songArray[counter]: " + counter);
 	}
